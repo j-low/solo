5b4d5c41266d168efbf2eef9e673d77ca842062b
Joe Lowinske <lowi0008@gmail.com>
2014-08-25 13:27:09 -0700

    Auth controller init build, Auth service scaffolding, signin html begin

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,5 +1,25 @@
 angular.module('commutify.auth', [])
 
 .controller('Auth', function($scope, $http, Auth) {
-  
+  $scope.signin = function() {
+  	Auth.signin($scope.user)
+  	.then(function(token) {
+      $window.localStorage.setItem('com.commutify', token);
+      $location('/index');
+  	})
+  	.catch(function(error) {
+      //TODO: INDICATE ERROR SOMEWHERE IN SIGNIN VIEW
+  	});
+  };
+
+  $scope.signup = function() {
+  	Auth.signup($scope.user)
+  	.then(function(token) {
+  		$window.localStorage.setItem('com.commutify', token);
+  		$location('/index');
+  	})
+  	.catch(function(error) {
+  		//TODO: INDICATE ERROR SOMEWHERE IN SIGNUP VIEW
+  	});
+  }; 
 });
---
+++ client/app/services/services.js
@@ -0,0 +1,18 @@
+angular.module('commutify.services', [])
+
+.factory('Auth', function($http, $location, $window) {
+	var signin = function() {};
+
+	var signup = function() {};
+
+	var signout = function() {};
+
+	var isAuth= function() {};
+})
+.factory('Input', function($scope, $http) {
+	var getAll = function() {};
+  
+  var get = function() {};
+
+	var post = function(input) {};
+})
